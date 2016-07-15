dc965614a7abeb7828f2c09e3a9d6db2c68847fb
Greg Allen <greg@firstandthird.com>
2016-07-15 16:15:23 -0700

    refactored typography.
    - created font-size mixing
    - updated typography.yaml to allow for a default size and then overrides
    (mobile)

--- styles/mixins/font-size.js
+++ styles/mixins/font-size.js
@@ -1,24 +1,23 @@
-'use strict';
-
-module.exports = function (config) {
-  return function () {
+/**
+ * @mixin font-size small|body|large|header-X
+ *
+ */
+module.exports = function(config) {
+  return function(mixin, size) {
     const styles = {};
+    //TODO: refactor to only output overrides - loop over fontSizes breakpoints
     const breakpoints = Object.keys(config.breakpoints);
 
     for (const breakpoint of breakpoints) {
       const bp = config.breakpoints[breakpoint].smallest ? null : `@media (min-width: ${config.breakpoints[breakpoint]['min-width']})`;
-      let block = styles;
 
       if (!config.breakpoints[breakpoint].smallest) {
         styles[bp] = {};
-        block = styles[bp];
       }
 
-      const classes = Object.keys(config.fontStyles[breakpoint]);
+      const sizes = config.fontSizes[breakpoint] || config.fontSizes.default;
 
-      for (const style of classes) {
-        block[`.${style}`] = config.fontStyles[breakpoint][style];
-      }
+      styles[bp] = sizes[size];
     }
 
     return styles;
