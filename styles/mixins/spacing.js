0d16a3d0edd7fa2e668b19ec85f36731d6f25fbb
Greg Allen <greg@firstandthird.com>
2016-07-18 10:28:25 -0700

    added bootstrap style helpers for padding, margin. example: p-t-lg to add large padding on top

--- styles/mixins/spacing.js
+++ styles/mixins/spacing.js
@@ -1,25 +1,41 @@
 'use strict';
 
 module.exports = function (config) {
-  return function (mixin, size, position) {
+  const spacingMixin = function(prop, position, size) {
     const styles = {};
-    position = position || 'bottom';
-    const breakpoints = Object.keys(config.breakpoints);
+    const spacingBreakpoints = Object.keys(config.spacing);
 
-    for (const breakpoint of breakpoints) {
-      const bp = config.breakpoints[breakpoint].smallest ? null : `@media (min-width: ${config.breakpoints[breakpoint]['min-width']})`;
+    for (const breakpoint of spacingBreakpoints) {
       let block = styles;
-
-      if (!config.breakpoints[breakpoint].smallest) {
-        styles[bp] = {};
-        block = styles[bp];
+      if (breakpoint !== 'default') {
+        const mediaquery = `@media (min-width: ${config.breakpoints[breakpoint]['min-width']})`;
+        styles[mediaquery] = {};
+        block = styles[mediaquery];
       }
 
       const spacings = config.spacing[breakpoint] || config.spacing.default;
 
-      block[`margin-${position}`] = spacings[size];
+      block[`${prop}-${position}`] = spacings[size];
     }
 
     return styles;
   };
+
+  return function (mixin, prop, position, size) {
+    if (prop && position && size) {
+      return spacingMixin(prop, position, size);
+    }
+    const styles = {};
+    const properties = ['padding', 'margin'];
+    const positions = ['top', 'left', 'right', 'bottom'];
+    const sizes = Object.keys(config.spacing.default);
+    properties.forEach((property) => {
+      positions.forEach((position) => {
+        sizes.forEach((size) => {
+          styles[`${property[0]}-${position[0]}-${size}`] = spacingMixin(property, position, size);
+        });
+      });
+    });
+    return styles;
+  }
 };
