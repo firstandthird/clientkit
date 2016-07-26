eee029a1ceb8d043d008188b2f4da41e735c99e7
Antonio Laguna <alaguna@google.com>
2016-07-26 23:13:15 +0200

    Moving things around
    #40

--- styles/mixins/bg-colors.js
+++ styles/mixins/bg-colors.js
@@ -3,14 +3,13 @@
 module.exports = function (config) {
   return function (mixin) {
     const styles = {};
-    const blacklist = ['input', 'background', 'link', 'button', 'border'];
-    const isBlacklisted = arr => blacklist.some(v => (arr.indexOf(v) >= 0));
+    const isWhitelisted = color => color.startsWith('background-');
     const colors = Object.keys(config.color);
     const getColorObject = color => ({ 'background-color': config.color[color] });
 
     colors.forEach(color => {
-      if (!isBlacklisted(color.split('-'))) {
-        styles[`.bg-${color}`] = getColorObject(color);
+      if (isWhitelisted(color)) {
+        styles[`.bg-${color.replace('background-', '')}`] = getColorObject(color);
       }
     });
 
