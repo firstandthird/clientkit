4cf415699057576efd58e86bc6e39b544c6a7c60
Newton Koumantzelis <chrisohpedia@gmail.com>
2016-08-03 17:11:41 -0700

    Update; fix tests and config properties

--- styles/mixins/text-stretch.js
+++ styles/mixins/text-stretch.js
@@ -1,6 +1,6 @@
 module.exports = function ( config ) {
   return function (mixin, size) {
-    size = size || config.fontStyles.default['text-stretch'];
+    size = size || config.fontStyles.default['text-stretch']['letter-spacing'];
     return {
         'letter-spacing' : size
     };
