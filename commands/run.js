1458fa3e8351fdf83ac46c32dfbdcbf22df20243
Chris <orthagonal1@gmail.com>
2016-08-10 12:51:51 -0500

    will update the styleguide when run in production mode

--- commands/run.js
+++ commands/run.js
@@ -1,5 +1,7 @@
 'use strict';
 const mkdirp = require('mkdirp');
+const styleguide = require('../commands/styleguide.js');
+const path = require('path');
 
 module.exports.runAll = (config, log) => {
   const cssProcessor = require('../tasks/css.js');
@@ -12,4 +14,10 @@ module.exports.runAll = (config, log) => {
   if (config.scripts) {
     Object.keys(config.scripts).forEach(script => jsProcessor(config, process.cwd(), script, config.scripts[script]));
   }
+  styleguide(
+    config,
+    path.join(__dirname, '..', 'lib', 'styleguide.template'),
+    path.join(config.core.dist, 'styleguide.html'),
+    log
+  );
 };
