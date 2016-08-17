f69c6982896379f8379dba42b0f97e668ca0264c
Greg Allen <greg@firstandthird.com>
2016-08-17 10:33:53 -0700

    added icon mixin

---
+++ styles/mixins/icon.js
@@ -0,0 +1,23 @@
+'use strict';
+const path = require('path');
+const pathExists = require('path-exists');
+module.exports = function(config) {
+  return function(rule, icon, size, fill) {
+    const iconpath = path.resolve(__dirname, '../../icons/ionicons/', `${icon}.svg`);
+
+    if (!pathExists.sync(iconpath)) {
+      throw new Error(`invalid icon: ${icon}`);
+    }
+    if (!fill) {
+      fill = config.color.icons;
+    }
+
+    return {
+      background: `svg-load('${iconpath}', fill=${fill});`,
+      'background-size': size,
+      display: 'inline-block',
+      height: size,
+      width: size
+    };
+  };
+};
