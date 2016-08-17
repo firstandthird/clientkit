2c4e268418533c3f73e873118de4884084fbe000
Greg Allen <greg@firstandthird.com>
2016-08-17 11:45:24 -0700

    support for local files

--- styles/mixins/icon.js
+++ styles/mixins/icon.js
@@ -3,11 +3,20 @@ const path = require('path');
 const pathExists = require('path-exists');
 module.exports = function(config) {
   return function(rule, icon, size, fill) {
-    const iconpath = path.resolve(__dirname, '../../icons/ionicons/', `${icon}.svg`);
-
-    if (!pathExists.sync(iconpath)) {
-      throw new Error(`invalid icon: ${icon}`);
+    if (!size) {
+      throw new Error('must pass in size for icon');
     }
+    const cssfile = rule && rule.source.input && rule.source.input.file;
+    let iconpath = path.resolve(cssfile, icon);
+
+    //check to see if icon is a path that exists
+    if (!pathExists.sync(iconpath)) {
+      iconpath = path.resolve(__dirname, '../../icons/ionicons/', `${icon}.svg`);
+      if (!pathExists.sync(iconpath)) {
+        throw new Error(`invalid icon: ${icon}`);
+      }
+    }
+
     if (!fill) {
       fill = config.color.icons;
     }
