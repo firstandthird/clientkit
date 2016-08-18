e065dee805d9205900685af3c3f64b2e6282fdf9
Newton Koumantzelis <chrisohpedia@gmail.com>
2016-08-17 21:45:42 -0700

    Update; convert to helper

--- test/test.mixins.js
+++ test/test.mixins.js
@@ -106,15 +106,6 @@ describe('hide', function() {
   });
 });
 
-describe('close', function() {
-  it('generates close classes', (done) => {
-    const close = require('../styles/mixins/close.js')(conf);
-    const result = close({});
-    compare(result, 'close.js');
-    done();
-  });
-});
-
 describe('input', function() {
   it('generates input classes', (done) => {
     const inputs = require('../styles/mixins/inputs.js')(conf);
