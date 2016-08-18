de344e655b5a94b94a017bc6d2bf6a6e49df0a20
Chris <orthagonal1@gmail.com>
2016-08-18 13:10:59 -0500

    updated out-of-date unit tests

--- test/expectedOutputs/mixins/inputs.js
+++ test/expectedOutputs/mixins/inputs.js
@@ -1,10 +1,17 @@
 module.exports={
-  ".input": {
-    "background": "#fff",
-    "border": "1px solid #333",
-    "font-size": "14px",
-    "line-height": "24px",
-    "padding": 0,
-    "margin": 0
-  }
+  "&:disabled": {
+     "border-bottom": "1px dashed gray",
+     "box-shadow": "none;",
+     "color": "gray",
+   },
+   "&:focus": {
+     "box-shadow": " 0 2px 0 0 blue",
+     "outline": "none"
+   },
+   "background-color": "#fff",
+   "border": 0,
+   "box-shadow": " 0 1px 0 0 #333",
+   "margin": 0,
+   "padding": 0,
+   "transition": "box-shadow ease 250ms"
 };
