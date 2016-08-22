e3cd74e7965d5bda5df9feb5dca262161e789932
Chris <orthagonal1@gmail.com>
2016-08-22 13:26:35 -0500

    tests default.css output with default config
    will let you know when a change you've made to clientkit affects the
    default outputs

---
+++ test/test.defaultCss.js
@@ -0,0 +1,31 @@
+'use strict';
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const expect = require('chai').expect;
+const path = require('path');
+const fs = require('fs');
+const cssModule = require('../tasks/css.js');
+
+const conf = require('confi')({
+  path: [
+    './conf'
+  ],
+  context: {
+    CKDIR: __dirname,
+    CONFIGDIR: path.join(process.cwd(), 'conf')
+  }
+});
+
+describe('default css output', function() {
+  this.timeout(15000);
+  it('detects dev changes that affect the expected CSS output', (done) => {
+    // generate css against ./styles/default.css
+    const cssTask = new cssModule.CssTask(conf, process.cwd());
+    cssTask.performTask('./styles/default.css', (result) => {
+      const expectedCssString = fs.readFileSync('test/expectedOutputs/default.css').toString();
+      expect(typeof result.css).to.equal('string');
+      expect(result.css).to.equal(expectedCssString);
+      done();
+    });
+  });
+});
