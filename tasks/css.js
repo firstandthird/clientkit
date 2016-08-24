ba01d1c5240c73747eea0dd98a1573b04ed5520b
Greg Allen <greg@firstandthird.com>
2016-08-24 09:34:28 -0700

    added plugin name to message log

--- tasks/css.js
+++ tasks/css.js
@@ -138,7 +138,7 @@ class CssTask {
     .then(result => {
       if (result.messages) {
         result.messages.forEach(message => {
-          log(['error'], message.text);
+          log(['error'], `${message.text} [${message.plugin}]`);
         });
       }
 
