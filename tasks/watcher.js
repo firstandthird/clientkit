6dda46abdc68e34d8b8af0445b904020ead638b6
Greg Allen <greg@firstandthird.com>
2016-11-06 19:53:06 -0800

    remove workaround code for watcher

--- tasks/watcher.js
+++ tasks/watcher.js
@@ -4,20 +4,8 @@ const ClientKitTask = require('../lib/task');
 const chokidar = require('chokidar');
 const debounce = require('lodash.debounce');
 const pathLib = require('path');
-const async = require('async');
 
 class WatcherTask extends ClientKitTask {
-  //TODO: remove when base is cleaned up
-  execute(allDone) {
-    if (!this.options.files) {
-      return allDone();
-    }
-    const filenames = Object.keys(this.options.files);
-    async.each(filenames, (filename, next) => {
-      this.process(this.options.files[filename], filename, next);
-    }, allDone);
-  }
-
   process(tasks, watch, done) {
     this.watcher = chokidar.watch(watch, {
       ignored: /node_modules|dist/,
