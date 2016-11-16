452ddc571197e914a81d46bd63d6e6e8efda1d1f
Chris <orthagonal1@gmail.com>
2016-11-16 10:22:16 -0600

    css tests

--- test/test.tasks.js
+++ test/test.tasks.js
@@ -3,12 +3,40 @@
 const expect = require('chai').expect;
 const taskLoader = require('../lib/load-tasks');
 const path = require('path');
-
+const fs = require('fs');
 const fileToInput = path.join(__dirname, 'tasks', 'testInput.txt');
 const fileToOutput = path.join(__dirname, 'tasks', 'testOutput.txt');
 
+const options = {
+  description: 'compile your css',
+  color: {
+    primary: '#336699'
+  },
+  spacing: {
+    default: '1px'
+  },
+  grid: {
+    h1: '2px'
+  },
+  easing: {
+    left: '3px'
+  },
+  breakpoints: {
+    b1: '2px'
+  },
+  dist: './dist',
+  assetPath: './',
+  minify: false,
+  docs: {
+    enabled: false
+  }
+};
+
+const files = {};
+files[fileToInput] = fileToOutput;
+
 describe('CSSSourceTask', function() {
-  this.timeout(5000);
+  this.timeout(10000);
   beforeEach((done) => {
     done();
   });
@@ -18,39 +46,11 @@ describe('CSSSourceTask', function() {
   });
 
   it('can be initialized and run', (done) => {
-    const conf = require('confi')({
-      path: [
-        './conf'
-      ],
-      context: {
-        CKDIR: __dirname,
-        CONFIGDIR: path.join(process.cwd(), 'examples/clientkit')
-      }
-    });
-
-    const files = {};
-    files[fileToInput] = fileToOutput;
     const config = {
       tasks: {
         css: {
           register: 'tasks/css.js',
-          options: {
-            description: 'compile your css',
-            files,
-            config: {
-              color: {
-                primary: '#336699'
-              }
-            },
-            color: {
-              primary: '#336699'
-            },
-            // core: {
-            //   watch: {
-            //     scripts: ['examples/scripts/sample1.js']
-            //   }
-            // }
-          }
+          options
         }
       }
     };
@@ -58,19 +58,15 @@ describe('CSSSourceTask', function() {
       expect(err).to.equal(null);
       expect(typeof runner).to.equal('object');
       expect(runner.tasks.css.options.description).to.include('compile');
-      const oldLog = console.log;
-      const logResults = [];
-      console.log = (data) => {
-        logResults.push(data);
-      };
       runner.run(['css']);
+      fs.writeFileSync(fileToInput, '.myClass: { background: primary; }', 'utf-8');
       setTimeout(() => {
-        console.log = oldLog;
-        console.log(logResults)
-        // expect(logResults.length).to.equal(1);
-        // expect(logResults[0]).to.include('The keyword \'const\'');
+        // const file2 = fs.readFileSync(fileToOutput);
+        // console.log(file2);
+      //   // expect(logResults.length).to.equal(1);
+      //   // expect(logResults[0]).to.include('The keyword \'const\'');
         done();
-      }, 2000);
+      }, 5000);
     });
   });
 });
