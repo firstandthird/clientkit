061953d82e7655c7fc39145ad1c9a31fa3af2e0f
Chris <orthagonal1@gmail.com>
2016-11-17 12:01:24 -0600

    js maps working and tests

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -6,7 +6,8 @@ const babelify = require('babelify');
 const shim = require('browserify-shim');
 const bes2015 = require('babel-preset-es2015');
 const uglifyify = require('uglifyify');
-
+const exorcist = require('exorcist');
+const path = require('path');
 class ScriptsTask extends ClientKitTask {
   process(input, filename, done) {
     const b = new Browserify({
@@ -27,8 +28,11 @@ class ScriptsTask extends ClientKitTask {
 
     if (this.options.minify) {
       currentTransform = currentTransform.transform(uglifyify, { global: true });
+      const result = currentTransform
+      .bundle()
+      .pipe(exorcist(`${path.join(this.options.dist, filename)}.map`));
+      return this.write(filename, result, done);
     }
-
     this.write(filename, currentTransform.bundle(), done);
   }
 }
