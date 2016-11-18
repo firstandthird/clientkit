9049ddbf84b224ff04df78fe4ae7ec0176c3b69b
Chris <orthagonal1@gmail.com>
2016-11-18 11:35:22 -0600

    nuke lib/urlHashes and bring it into hash.js

--- tasks/hash.js
+++ tasks/hash.js
@@ -2,10 +2,31 @@
 const async = require('async');
 const ClientKitTask = require('clientkit-task');
 const glob = require('glob');
-const hasher = require('../lib/urlHashes.js');
+const hash = require('rev-hash');
+const path = require('path');
 const fs = require('fs');
+const debounce = require('lodash.debounce');
 
 class HashTask extends ClientKitTask {
+
+  constructor(name, config, runner) {
+    super(name, config, runner);
+    // map of original filename to its hashed equivalent, useful for url hashing:
+    this.map = {};
+  }
+
+  // hash the files, then save the filename:hashname map to file:
+  execute(allDone) {
+    this.clearMap();
+    super.execute((err, result) => {
+      if (err) {
+        return allDone(err);
+      }
+      this.writeMap();
+      return allDone(null, result);
+    });
+  }
+
   process(input, filename, processDone) {
     async.auto({
       filesToHash: (done) => {
@@ -15,11 +36,40 @@ class HashTask extends ClientKitTask {
       hashNames: ['filesToHash', (results, done) => {
         const hashMap = {};
         results.filesToHash.forEach((file) => {
-          fs.renameSync(file, hasher.hash(file, fs.readFileSync(file)));
+          const hashName = this.hasher(file, fs.readFileSync(file));
+          this.map[file] = hashName;
+          fs.renameSync(file, hashName);
         });
         done(null, hashMap);
       }],
     }, processDone);
+  }
+
+  hasher(inputName, inputContent) {
+    let hashKey;
+    if (typeof inputContent === 'string') {
+      hashKey = hash(Buffer.from(inputContent));
+    } else {
+      hashKey = hash(inputContent);
+    }
+    const extension = path.extname(inputName);
+    const basename = path.basename(inputName);
+    const outputName = inputName.replace(extension, `.${hashKey}${extension}`);
+    const baseOutput = basename.replace(extension, `.${hashKey}${extension}`);
+    this.map[basename] = baseOutput;
+    return outputName;
+  }
+
+  // write the url hash map, only once every debounce seconds:
+  writeMap() {
+    debounce(() => {
+      fs.writeFileSync(this.options.urlHashing.jsonMapping, JSON.stringify(this.map));
+    }, this.options.delay)();
+  }
+
+  // clear the url hash map:
+  clearMap() {
+    this.map = {};
   }
 }
 module.exports = HashTask;
