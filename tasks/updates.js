7dd552214b42005984fb3043f482818347daec55
Chris <orthagonal1@gmail.com>
2016-11-21 19:01:45 -0600

    update task

---
+++ tasks/updates.js
@@ -0,0 +1,20 @@
+'use strict';
+const ClientKitTask = require('clientkit-task');
+const updateNotifier = require('update-notifier');
+const pkg = require('../package.json');
+
+class UpdateTask extends ClientKitTask {
+  get description() {
+    return 'Checks to see if an updated version of Clientkit exists';
+  }
+
+  execute(done) {
+    // notify if a more-recent clientkit is available:
+    const result = updateNotifier({ pkg, updateCheckInterval: 1 }).notify();
+    if (result.update && result.update.latest !== result.update.current) {
+      this.log(['warning'], `A new version of clientkit is available on npm! Current: ${result.update.current}  Latest: ${result.update.latest}`);
+    }
+    done();
+  }
+}
+module.exports = UpdateTask;
