1191608f661393105b38701ac0faf55ff4844860
Chris <orthagonal1@gmail.com>
2017-01-02 18:59:58 -0600

    pass conf/plugin directories to core

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const path = require('path');
 const main = require('clientkit-core');
-main();
+
+// clientkit-core can be given a list of additional directories containing
+// configuration files and a list of additional plugin directories
+main({
+  confDirectories: [path.join(__dirname, 'conf')],
+  pluginDirectories: [path.join(__dirname, 'tasks')]
+});
