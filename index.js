0499a3bd78d5853af03c425690c7ec3b17ba6ed7
Alejandro Pérez Martín <aleperez92@gmail.com>
2018-12-13 19:14:03 +0100

    Rename index script

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-#!/usr/bin/env node
-
-const main = require('taskkit');
 const path = require('path');
+const webpack = require('webpack');
+const getConfig = require('./lib/get-config');
 
-process.env.TASKKIT_PREFIX = 'clientkit';
-process.env.TASKKIT_BASECONFIG = path.join(__dirname, 'conf');
-process.env.TASKKIT_CKDIR = __dirname;
-const task = process.argv[2] || 'default';
+process.env.CK_PREFIX = 'default';
+process.env.CK_BASE_CONFIG = path.join(__dirname, 'conf');
+process.env.CK_CONFIG = path.join(__dirname, 'conf');
+process.env.CK_PATH = __dirname;
 
-
-const run = async function() {
+const run = async () => {
   try {
-    await main(task);
+    const config = await getConfig();
+    console.log(config);
+    webpack(config, (err, stats) => {
+      if (err || stats.hasErrors()) {
+        // Handle errors here
+        console.log(err);
+      }
+      console.log('FINISHED');
+      // Done processing
+    });
   } catch (e) {
+    console.log(e);
     process.exit(1);
   }
 };
