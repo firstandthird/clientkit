57a2a67b826a2578c629148db1b78ddbed578675
Antonio Laguna <sombragriselros@gmail.com>
2018-12-14 18:33:00 +0100

    Fixing scripts from npm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const getConfig = require('./lib/get-config');
 
-process.env.CK_PREFIX = 'default';
+process.env.CK_PREFIX = 'clientkit';
 process.env.CK_BASE_CONFIG = path.join(__dirname, 'conf');
 process.env.CK_CONFIG = path.join(__dirname, 'conf');
 process.env.CK_PATH = __dirname;
