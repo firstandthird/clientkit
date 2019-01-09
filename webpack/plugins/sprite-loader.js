5aeda2aae5b18947b73de6fb86fcfe7b52b58824
Alejandro Pérez Martín <AlejandroPerezMartin@users.noreply.github.com>
2019-01-09 11:59:02 +0100

    Allow babel-loader access node_modules (#204)
    * Allow babel-loader access node_modules
    
    * Updating deps, adding testcase
    
    * Triggering new build
    
    * Some fixes
    
    * Fixing query

--- webpack/plugins/sprite-loader.js
+++ webpack/plugins/sprite-loader.js
@@ -1,3 +1,10 @@
 const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
-module.exports = new SpriteLoaderPlugin();
+module.exports = new SpriteLoaderPlugin({
+  plainSprite: true,
+  spriteAttrs: {
+    width: '0',
+    height: '0',
+    style: 'position:absolute;'
+  }
+});
