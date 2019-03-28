c52d22610429db4ba888af1b461525773cd587cc
Alejandro Pérez Martín <AlejandroPerezMartin@users.noreply.github.com>
2019-03-28 20:32:51 +0100

    Reload webpack when config files change (#206)
    * Upgrading libraries
    
    * Update clean plugin config
    
    * Reload webpack when config changes
    
    * Only run watcher on 'dev' tasks
    
    * Restart webpack on conf file change

---
+++ webpack.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+const getConfig = require('./webpack/get-config');
+const chokidar = require('chokidar');
+const webpack = require('webpack');
+const paths = require('./paths');
+
+const runWebpack = async function () {
+  let config = null;
+
+  try {
+    config = await getConfig();
+  } catch (error) {
+    console.log('There was an error getting the config!');
+    console.log(error);
+    process.exit(1);
+  }
+
+  try {
+    webpack(config, (err, stats) => {
+      if (err) {
+        console.error(err.stack || err);
+
+        if (err.details) {
+          console.error(err.details);
+        }
+
+        process.exit(1);
+      }
+
+      console.log(stats.toString({
+        chunks: false,
+        colors: true
+      }));
+
+      if (err || stats.hasErrors()) {
+        process.exit(1);
+      }
+    });
+  } catch (error) {
+    console.log('There was an error running clientkit!');
+    console.log('Config was:');
+    console.log(config);
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const run = () => {
+  runWebpack();
+
+  if (paths.isDevTask) {
+    try {
+      const watcher = chokidar.watch('./**/*.yaml', {
+        cwd: paths.baseConfig,
+        ignoreInitial: true,
+        interval: 300,
+        persistent: true
+      });
+
+      const events = ['ready', 'add', 'change', 'unlink'];
+
+      watcher.on('all', event => {
+        if (events.includes(event)) {
+          if (process.send) {
+            process.send('Child process finished');
+          }
+        }
+      });
+    } catch (error) {
+      console.log('There was an error running config watcher!');
+      console.log(error);
+      process.exit(1);
+    }
+  }
+};
+
+run();
