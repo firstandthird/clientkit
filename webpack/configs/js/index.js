5b6e264d3228ab1c6b871397b35a21cb5c80bdf8
Antonio Laguna <sombragriselros@gmail.com>
2019-04-03 11:17:46 +0200

    More optimizations

--- webpack/configs/js/index.js
+++ webpack/configs/js/index.js
@@ -26,7 +26,12 @@ module.exports = config => {
       path: config.scripts.dist || config.dist,
       filename: paths.isProduction ? '[name].[contenthash].js' : '[name].js'
     },
-    plugins: [],
+    plugins: [
+      '@babel/plugin-syntax-dynamic-import',
+      [
+        '@babel/plugin-transform-runtime', { regenerator: true }
+      ]
+    ],
     mode: paths.isProduction ? 'production' : 'development'
   };
 
