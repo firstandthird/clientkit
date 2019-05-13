186683048aab9d29fb90f2e7757fa83d9556197b
Antonio Laguna <sombragriselros@gmail.com>
2019-05-13 11:31:35 +0200

    Fixing CSS setting

--- webpack/plugins/extract-css.js
+++ webpack/plugins/extract-css.js
@@ -2,6 +2,6 @@ const ExtractCssChunks = require('extract-css-chunks-webpack-plugin');
 const fileNameGetter = require('../file-name-getter');
 
 module.exports = (options = {}) => new ExtractCssChunks({
-  fileName: fileNameGetter(options, '[name].css', '[name].[contenthash].css'),
+  filename: fileNameGetter(options, '[name].css', '[name].[contenthash].css'),
   chunkFilename: '[id]'
 });
