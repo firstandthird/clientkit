bcb15b1cd310a63337e5b99c308cf218febea95a
Troy Whiteley <troy@somany.us>
2019-07-16 02:24:04 -0700

    fixed clean plugin

--- webpack/plugins/clean-output.js
+++ webpack/plugins/clean-output.js
@@ -1,3 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = () => new CleanWebpackPlugin();
+module.exports = () => new CleanWebpackPlugin({
+  verbose: true,
+  cleanOnceBeforeBuildPatterns: [] // Prevents files from being removed that shouldnt
+});
