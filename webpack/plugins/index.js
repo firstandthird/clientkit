59f1d5822305b7efbb3a63648a8b88ab259b5dbe
Alejandro Pérez Martín <AlejandroPerezMartin@users.noreply.github.com>
2020-10-26 09:25:23 +0000

    Add stylelint linter (#228)
    * Update demo dependencies
    
    * Add stylelint linter, bump dependencies
    
    * Fix stylelint errors
    
    * Update tests result
    
    * Update dependencies

--- webpack/plugins/index.js
+++ webpack/plugins/index.js
@@ -3,11 +3,13 @@ const extractCss = require('./extract-css');
 const spriteLoader = require('./sprite-loader');
 const fixStyleEntries = require('./fix-style-entries');
 const assetsManifest = require('./assets-manifest');
+const lintCss = require('./lint-css');
 
 module.exports = {
   cleanOutput,
   fixStyleEntries,
   extractCss,
   spriteLoader,
+  lintCss,
   assetsManifest
 };
