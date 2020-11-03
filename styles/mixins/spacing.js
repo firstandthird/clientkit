fc838a73c9a0f342f13351c73ea073ea6e472083
Antonio Laguna <sombragriselros@gmail.com>
2020-11-03 12:54:42 +0100

    Clientkit 6.0 (#232)
    * Changing the spacing
    
    * Allowing to set these through config
    
    * Housekeeping
    
    * Adding new vars removing some
    
    * Workable common animation
    
    * Configurable button padding
    
    * Removing unneeded modules
    
    * Removing leftover comment
    
    * Button padding configurable
    
    * Removing some mixins
    
    * Housekeeping
    
    * Proper typography settings
    
    * Spacing and grid changes
    
    * Test updating

--- styles/mixins/spacing.js
+++ styles/mixins/spacing.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const breakpointHelper = require('../../lib/breakpoint-helper');
+
 module.exports = function (config) {
   const spacingMixin = function(prop, position, size) {
     const styles = {};
@@ -8,26 +9,40 @@ module.exports = function (config) {
     const axials = {
       xaxis: ['left', 'right'],
       yaxis: ['top', 'bottom'],
-      all: ['top', 'bottom', 'left', 'right']
+      all: ['']
     };
     let positions = [position];
     if (axials[position]) {
       positions = axials[position];
     }
     const allPositions = {};
-    positions.forEach((curPosition, index) => {
+
+    positions.forEach(curPosition => {
       spacingBreakpoints.forEach(breakpoint => {
-        if (typeof config.spacing[breakpoint][size] === 'undefined') {
+        const hasNoMatchingSize = typeof config.spacing[breakpoint][size] === 'undefined';
+        // Do not generate extra markup to behave similarly as default
+        const sizeIsDifferentThanDefault = !hasNoMatchingSize
+          && breakpoint !== 'default'
+          && config.spacing[breakpoint][size] === config.spacing.default[size];
+
+        if (hasNoMatchingSize || sizeIsDifferentThanDefault) {
           return;
         }
 
         if (!styles[breakpoint]) {
           styles[breakpoint] = {};
         }
-        styles[breakpoint][`${prop}-${curPosition}`] = `${config.spacing[breakpoint][size]}`;
+
+        if (curPosition) {
+          styles[breakpoint][`${prop}-${curPosition}`] = `${config.spacing[breakpoint][size]}`;
+        } else {
+          styles[breakpoint][prop] = `${config.spacing[breakpoint][size]}`;
+        }
       });
     });
+
     Object.assign(allPositions, breakpointHelper(styles, config));
+
     return allPositions;
   };
 
@@ -42,20 +57,48 @@ module.exports = function (config) {
     const properties = ['padding', 'margin'];
     const positions = ['top', 'left', 'right', 'bottom', 'xaxis', 'yaxis'];
     const sizes = Object.keys(config.spacing.default);
-    // first add the classes of the form <propery>-<size>:
-    properties.forEach((property) => {
-      sizes.forEach((curSize) => {
+
+    // first add the classes of the form <property>-<size>:
+    properties.forEach(property => {
+      sizes.forEach(curSize => {
         styles[`.${property}-${curSize}`] = spacingMixin(property, 'all', curSize);
       });
     });
+
     // then add the ones of the form <property>-<position>-<size>:
-    properties.forEach((property) => {
-      positions.forEach((positionString) => {
-        sizes.forEach((curSize) => {
-          styles[`.${property}-${positionString}-${curSize}`] = spacingMixin(property, positionString, curSize);
+    properties.forEach(property => {
+      positions.forEach(positionString => {
+        sizes.forEach(curSize => {
+          let selector = `.${property}-${positionString}-${curSize}`;
+
+          if (property === 'margin' && positionString === 'bottom') {
+            selector = `.spacing-${curSize}`;
+          }
+
+          styles[selector] = spacingMixin(property, positionString, curSize);
         });
       });
     });
+
+    const breakpoints = Object.keys(config.breakpoints);
+    const responsiveSpacing = {};
+
+    for (const breakpoint of breakpoints) {
+      if (!config.breakpoints[breakpoint].default) {
+        responsiveSpacing[breakpoint] = {};
+        const block = responsiveSpacing[breakpoint];
+
+        sizes.forEach(curSize => {
+          const selector = `.spacing-${breakpoint}-${curSize}`;
+          block[selector] = {
+            'margin-bottom': curSize === 'none' ? '0' : config.spacing.default[curSize]
+          };
+        });
+      }
+    }
+
+    Object.assign(styles, breakpointHelper(responsiveSpacing, config));
+
     return styles;
   };
 };
--- webpack/plugins/lint-css.js
+++ webpack/plugins/lint-css.js
@@ -2,14 +2,14 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = options => {
   const context = options.stylesheets.assetPath;
-  const shoudlWarn = !options.stylelint.failOnError || !options.failOnError;
+  const shouldWarn = !options.stylelint.failOnError || !options.failOnError;
   const shouldFail = options.stylelint.failOnError || options.failOnError;
 
   return new StylelintPlugin({
-    configFile: `${options.CKDIR}/package.json`,
-    ignorePath: `${options.CKDIR}/.stylelintignore`,
+    configFile: options.stylelint.configFile,
+    ignorePath: options.stylelint.ignorePath,
     context,
-    emitWarning: shoudlWarn,
+    emitWarning: shouldWarn,
     failOnWarning: shouldFail,
     failOnError: shouldFail
   });
